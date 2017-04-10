d19cf0b9c6b65412a9806f742336eb442ce7d3d7
Joab Torres <joabtorres1508@gmail.com>
2017-04-10 08:56:38 -0300

    Desenvolvimento: galeria, noticias, contato

--- js/cooi_script.js
+++ js/cooi_script.js
@@ -27,3 +27,42 @@ $(document).ready(function () {
     $().UItoTop({easingType: 'easeOutQuart'});
 
 });
+
+$(document).ready(function () {
+
+    var defaults = {
+        containerID: 'toTop', // fading element id
+        containerHoverID: 'toTopHover', // fading element hover id
+        scrollSpeed: 1200,
+        easingType: 'linear'
+    };
+
+
+    $().UItoTop({easingType: 'easeOutQuart'});
+
+});
+$(document).ready(function ($) {
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1200);
+    });
+});
+
+if (document.getElementById("mapa_contato")) {
+    function init_map1() {
+        var myLocation = new google.maps.LatLng(-4.222738, -56.001706);
+        var mapOptions = {
+            center: myLocation,
+            zoom: 16
+        };
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            title: "Localização dos imoveis da Kananda Imobiliaria!",
+            icon: "imagens/marcado.png"
+        });
+        var map = new google.maps.Map(document.getElementById("mapa_contato"),
+                mapOptions);
+        marker.setMap(map);
+    }
+    init_map1();
+}
